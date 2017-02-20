d1b8a9a425b2b40b2683d742da82f285f30e5dfc
Nicco Narbutas <nnarb001@cslab-123.cs.plattsburgh.edu>
2017-02-20 11:29:44 -0500

    elseif statement

--- drinkRink.c
+++ drinkRink.c
@@ -8,26 +8,22 @@ int main(){
 char *ingredient;
 int usrinput;
 
-printf("Press 1 if you want to enter an ingredient, if not press 2: ");
-scanf("%d", &usrinput);
 
-
-if (usrinput == 1){
-	printf("Enter an ingredient: ");
-	scanf("%s", &ingredient);
-	printf("\nyou entered %s\n\n", &ingredient);
+while(1){
+	printf("Press 1 if you want to enter an ingredient, if not press 2: ");
+	scanf("%d", &usrinput);
+	if (usrinput == 1){
+		printf("Enter an ingredient: ");
+		scanf("%s", &ingredient);
+		printf("\nyou entered %s\n\n", &ingredient);
+		}
+	else if (usrinput == 2){
+		break;
+		}
+	else {
+		printf("\nYou entered an invalid number!!!\n\n");
+		}
 }
-
-
-
-
-
-
-
-
-
-
-
 
 return 0;
 
