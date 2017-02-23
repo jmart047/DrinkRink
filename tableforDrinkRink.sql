c82986bc773aeff105a375d766e5be8532927b9a
pboca001 <pboca001@plattsburgh.edu>
2017-02-23 12:41:58 -0500

    Added data

--- tableforDrinkRink.sql
+++ tableforDrinkRink.sql
@@ -15,13 +15,15 @@ USE `classicmodels`;
 DROP TABLE IF EXISTS `drinks`;
 
 CREATE TABLE `drinks` (
-  `DrinkName` int(11) NOT NULL,
+  `DrinkName` varchar(50) NOT NULL,
   `Ing_1` varchar(50) NOT NULL,
   `Ing_2` varchar(50) NOT NULL,
+  `Ing_3` varchar(50) NOT NULL,
   `Ing_4` varchar(50) NOT NULL,
   `Ing_5` varchar(50) NOT NULL,
   `Ing_6` varchar(50) NOT NULL,
   `Ing_7` varchar(50) NOT NULL,
+  `Ing_8` varchar(50) NOT NULL,
   `Ing_9` varchar(50) NOT NULL,
   `Ing_10` varchar(50) NOT NULL,
   PRIMARY KEY (`DrinkName`),
@@ -31,4 +33,22 @@ CREATE TABLE `drinks` (
 
 /*Data for the table `drinks` */
 
-insert  into `drinks`(`Ing_1`,`Ing_2`,`Ing_3`,`Ing_4`,`Ing_5`,`Ing_7`,`Ing_8`,`Ing_9`,`Ing_10`) values 
+insert  into `drinks`(`DrinkName`,`Ing_1`,`Ing_2`,`Ing_3`,`Ing_4`,`Ing_5`,`Ing_6`,`Ing_7`,`Ing_8`,`Ing_9`,`Ing_10`) values 
+
+
+('Mojito','50 ml White Rum','8 Mint leaves',' 12 1/2 ml Sugar Syrup','25 ml Lime Juice','2 Mint springs',NULL,NULL,NULL,NULL,NULL,),
+('Martini','50 ml Gin','10 ml Dry Vermouth','Lemon twist',NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+('Daiquiri','50 ml White Rum','1 1/2 bar spoons Sugar','25 ml Lime Juice','Lime slice',NULL,NULL,NULL,NULL,NULL,NULL),
+('Old Fashioned','50 ml Bourbon','2 dashes Angostura Bitters','1 dash Orange Bitters','1 bar spoon Sugar','Orange peel',NULL,NULL,NULL,NULL,NULL),
+('Cosmopolitan','37 1/2 ml Citrus Vodka','12 1/2 ml Triple Sec','30 ml Cranberry Juice','1/2 squeezed lemon','12 1/2 ml Sugar Syrup',NULL,NULL,NULL,NULL,NULL),
+('Caipirinha','50 ml Cachaca','1/2 Lime','1 teaspoon Brown Sugar','Lime Wedge',NULL,NULL,NULL,NULL,NULL,NULL),
+('White Russian','37 1/2 ml Vodka','25 ml Coffee Liqueur','15 ml Cream','15 ml Milk','Chocolate Powder','Coffee Bean',NULL,NULL,NULL,NULL),
+('Pina Colada','50 ml White Rum','100 ml Pineapple Juice','25 ml Coconut Cream','25 ml Cream','Pineapple Slice','pinch of Salt',NULL,NULL,NULL,NULL),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
+('','','','','','','','','','',''),
