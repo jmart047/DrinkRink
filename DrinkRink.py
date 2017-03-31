99ca5cd234d1b44a178f7dd0788f237e131a9b8d
pboca001 <pboca001@plattsburgh.edu>
2017-03-31 11:08:57 -0400

    Add files via upload

--- DrinkRink.py
+++ DrinkRink.py
@@ -4,22 +4,27 @@
 
 import sqlite3
 
-conn = sqlite3.connect('DrinkRinkRealVersion.sqlite')
+conn = sqlite3.connect('DrinkRink1.db')
 c = conn.cursor()
 
 
 def checkIngredient():
     usrIngredient = input("\nEnter your ingredient\n\n")
     
-    results = c.execute("SELECT DRDESC FROM Drinks WHERE DRDESC = ? ", (usrIngredient,))
+    results = c.execute("SELECT ININID FROM Ingredients WHERE INDESC = ?", (usrIngredient,))
 
     resultrow = c.fetchone()
 
     if resultrow is not None:
         print(resultrow)
     else:
-        print("Sorry, there are no drinks with that ingredient")
-
-checkIngredient()    
-
-
+        print("Sorry, thats not an ingredient")
+   
+    for row in resultrow:
+        newresult = row
+        
+        dresult = c.execute("SELECT DTDRID FROM Detail WHERE INGID = ?", (newresult,))
+        dresult= c.fetchall()
+        if dresult is not None:
+            print(dresult)
+checkIngredient()   
