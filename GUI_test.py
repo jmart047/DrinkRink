85387d61657ef9c7c571aa4ca318c09bed23afbd
pboca001 <pboca001@plattsburgh.edu>
2017-04-09 18:49:56 -0400

    Add files via upload

--- GUI_test.py
+++ GUI_test.py
@@ -11,16 +11,43 @@ c = conn.cursor()
 top = Tk()
 top.geometry("300x300")
 top.title("DrinkRink")
+
 def test():
-    L1 = Label(top, text = "Ingredient:")
-    L1.grid( row=1, column=5)
-    E1 = Entry(top, bd = 5)
-    E1.grid(row = 9, column =5)
+    t=Toplevel()
+    label1= Label(t,text="enter ing")
+    e1= Entry(t, bd = 5)
+    def getIng():
+        ing= e1.get()
+        result =c.execute("SELECT ININID FROM Ingredients WHERE INDESC = ?", (ing,))
+        resultrow = c.fetchone()
+##        if resultrow is not None:
+##            print(resultrow)
+##        else:
+##            print("Sorry, thats not an ingredient")
+   
+        for row in resultrow:
+            newresult = row
+        
+            dresult = c.execute("SELECT DTDRID FROM Detail WHERE INGID = ?", (newresult,))
+            dresult= c.fetchall()
+            t2=Toplevel(t)
+            mess= Message(t2, text=dresult)
+            mess.pack()
+        
+    button1= Button(t, text="Enter", command= getIng)
+    label1.pack()
+    e1.pack()
+    button1.pack()
+    button = Button(t, text="Dismiss", command=top.destroy)
+    button.pack()
 def drinklist():
-    a = c.execute("SELECT * FROM Drinks ")
+    a = c.execute("SELECT DRDESC FROM Drinks ")
     b = c.fetchall()
-    print(b)
     top = Toplevel()
+    msg = Message(top, text=b)
+    msg.pack()
+    button = Button(top, text="Dismiss", command=top.destroy)
+    button.pack()
 
    
 
